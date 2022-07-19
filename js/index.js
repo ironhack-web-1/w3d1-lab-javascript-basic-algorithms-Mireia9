1e929062f3bc1ed0e11d8041b5bbebbb7513d518
John Doe <mbertran9@gmail.com>
2022-07-19 20:43:05 +0200

    iteteration 1

--- js/index.js
+++ js/index.js
@@ -1,5 +1,10 @@
 // Iteration 1: Names and Input
-//
+
+let driverName = "hacker1";
+console.log("The driver's name is" + driverName);
+let navigatorName = "hacker2";
+console.log("The navigator's name is" + navigatorName);
+
 // Iteration 2: Conditionals
 
 
